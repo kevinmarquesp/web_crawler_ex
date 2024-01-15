4ed62415f470f8186aef23d8c59f9b769cbe2eda
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-15 00:36:10 -0300

    :recycle: rewritten the code of the http fetch module to be more... functional?

--- lib/web_crawler_ex.ex
+++ lib/web_crawler_ex.ex
@@ -1,11 +1,11 @@
 defmodule WebCrawlerEx do
-  # @db_file "results.sqlite3"
+  @db_file "results.sqlite3"
 
   def main(argv) do
     Enum.each(argv, fn user_url ->
       valid_urls = WebCrawlerEx.HandleHttpRequests.get_inner_links(user_url)
-                   |> Enum.uniq()
       Enum.each(valid_urls, &(IO.puts(&1)))
+      Enum.each(valid_urls, &(IO.puts(is_list &1)))
     end)
   end
 end
@@ -26,26 +26,24 @@ defmodule WebCrawlerEx.HandleHttpRequests do
   def get_inner_links(base_url) do
     {:ok, page_html} = fetch(base_url)  #todo: handle error
     href_values = get_href_values(page_html)  #todo: handle error
+
     filter_valid_links(href_values)
   end
 
-  #pattern matching estranho que dá pra por em funções anônimas (o que é dahora)
-  defp filter_valid_links(href_values) do
-    Enum.filter(href_values, fn
-      [href_value] -> valid_http_url?(href_value)
-      _ -> false
-    end)
-  end
+  defp filter_valid_links(href_values), do:
+    Enum.filter(href_values, &valid_http_url?/1)
 
-  defp valid_http_url?(url_str) do
-    valid_url_exp = ~r/^http(s)?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-    Regex.match?(valid_url_exp, url_str)
-  end
+  defp valid_http_url?(url_str), do:
+    ~r/^http(s)?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+    |> Regex.match?(url_str)
 
   defp get_href_values(page_html) do
     {:ok, parsed_html} = Floki.parse_document(page_html)  #todo: handle error
     a_tags = Floki.find(parsed_html, "body a")
+
     Enum.map(a_tags, &(Floki.attribute(&1, "href")))
+    |> Enum.map(&hd/1)
+    |> Enum.uniq()
   end
 
   defp fetch(url) do
