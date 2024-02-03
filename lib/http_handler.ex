123afa99f60be31ea5e22117877b72446e19adbd
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-03 17:04:46 -0300

    :bulb: docs-code (http handler): added some docstrings to make the code a little more understandable

--- lib/http_handler.ex
+++ lib/http_handler.ex
@@ -17,6 +17,12 @@ defmodule WebCrawlerEx.HTTPHandler do
     end
   end
 
+  @doc """
+  Extracts the value of a given attribute from the HTML body. This function uses
+  the Floki library to parse the HTML and find the attribute. If the parsing is
+  successful, it returns a tuple {:ok, attribute_value}. If the parsing fails,
+  it returns a tuple {:error, reason}.
+  """
   def extract_attribute(body, attribute) do
     {status, result} = Floki.parse_document(body)
 
@@ -30,13 +36,26 @@ defmodule WebCrawlerEx.HTTPHandler do
       }
     end
   end
-
+  
+  @doc """
+  Extracts all URLs from a given text body. This function uses a regular expression
+  to find all strings that match the pattern of a URL. It returns a list of URLs
+  without any fragment identifiers (the part of the URL after the '#').
+  """
   def extract_urls(body) do
     Regex.scan(~r/http(s)?:\/\/[\w\.\/\-=?#]+/, body)
     |> Enum.map(&Enum.at(&1, 0))
     |> Enum.map(&Enum.at(String.split(&1, "#"), 0))
   end
 
+  
+  @doc """
+  Extracts the domain from a given URL. This function uses the URI.parse function
+  to parse the URL and extract the scheme (like 'http' or 'https') and the host
+  (the domain name). If the URL is valid and contains a scheme and a host, it returns
+  a tuple {:ok, domain_name}. If the URL is not valid or does not contain a scheme
+  and a host, it returns {:error, :noturl}.
+  """
   def get_domain(url) do
     case URI.parse(url) do
       %URI{scheme: scheme, host: host} when is_binary(scheme) and is_binary(host) ->
