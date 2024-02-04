02b98162faa8904dccac958a33687deddc27cb70
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-03 21:34:41 -0300

    :bulb: docs-code (controller): a simple docstring added to the fetch inner urls controller function

--- lib/web_crawler_ex.ex
+++ lib/web_crawler_ex.ex
@@ -4,6 +4,16 @@ defmodule WebCrawlerEx do
 
   @html_url_attributes ["href", "src"]
 
+  @doc """
+  Fetches all inner URLs from a given base URL. This function first extracts the
+  domain from the base URL using the `HTTPHandler.get_domain/1` function. The function
+  returns a list of URLs without any fragment identifiers (no `#` in the url nor
+  `/` character at the end).
+
+  If the base URL is not valid or does not contain a domain, it returns an error
+  tuple with the reason. If the body of the base URL cannot be fetched or is not
+  text-based content, it also returns an error/warning tuple with the reason.
+  """
   def fetch_inner_urls(base_url) do
     case HTTPHandler.get_domain(base_url) do
       {:ok, domain} ->
