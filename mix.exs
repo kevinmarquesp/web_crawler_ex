e272161218c60f31cf36a19b70a283085d2769e6
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-08 17:58:30 -0300

    :package: dep-up (debugging): the :observer library is available on iex for this project

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule WebCrawlerEx.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :eex, :wx, :observer, :runtime_tools]
     ]
   end
 
