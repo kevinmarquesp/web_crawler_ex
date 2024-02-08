5d8ac33d233b5c92054fc269ed70ca96ada84824
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-02-08 18:02:45 -0300

    :sparkles: feature (crawler): added the base code that do the recursive crawling and store it all on database

--- lib/web_crawler_ex.ex
+++ lib/web_crawler_ex.ex
@@ -1,62 +1,98 @@
 defmodule WebCrawlerEx do
   alias WebCrawlerEx.Http.ExtractInnerUrls
+  alias WebCrawlerEx.Db.Init
   require Logger
 
+  @db "./.db.sqlite3"
+  @migration_file "./migrate.sql"
+
+  @url_buffer_table :url_buffer
+  @valid_result_table :valid_result
+  @binnary_result_table :binnary_result
+  @timeout_error_table :timeout_error
+
   @timeout_span 10_000
-  
+
   @doc """
   """
-  def init(_url) do
+  def init_crawnler(init) do
+    case Exqlite.Basic.open(@db) do
+      {:ok, conn} ->
+        execute_migration(conn, init)
+      {:error, reason} ->
+        Logger.error("#{inspect(self())} Couldn't open the #{@db} file: #{reason}")
+    end
+  end
+
+  defp execute_migration(conn, init) do
+    case Init.migrate(conn, @migration_file) do
+      :ok ->
+        recursive_crawnler(conn, init)
+      {:error, reason} ->
+        Logger.error("#{inspect(self())} Error reading file #{@migration_file}: #{reason}")
+    end
   end
 
   @doc """
   """
-  def crawn([]), do:
-    Logger.warning("#{inspect(self())} Cannot handle an empty list")
+  def recursive_crawnler(conn, init) do
+    crawnler(conn, init)
 
-  def crawn(base_url) when is_list(base_url) do
-    pid = inspect(self())
+    case Init.select(conn, @url_buffer_table) do
+      {:ok, urls} ->
+        recursive_crawnler(conn, urls)
+      {:error, reason} ->
+        Logger.error("#{inspect(self())} #{reason}")
+    end
+  end
 
-    urls = base_url
-    |> Enum.map(&Task.async(fn -> crawn(&1) end))
-    |> Enum.flat_map(fn task ->
+  @doc """
+  """
+  def crawnler(_, []), do: Logger.error("#{inspect(self())} Cannot handle an empty list!")
+
+  def crawnler(conn, base_urls) when is_list(base_urls) do
+    base_urls
+    |> Enum.map(&Task.async(fn -> crawnler(conn, &1) end))
+    |> Enum.map(fn task ->
       try do
         Task.await(task, @timeout_span)
       catch
         :exit, _ ->
-          Logger.error("#{pid} Task timeout: #{inspect(task)}")
-          []
+          Logger.error("#{inspect(self())} Error during processing task")
       end
     end)
+  end
+  
+  def crawnler(conn, base_url) do
+    case ExtractInnerUrls.extract_inner_urls(base_url) do
+      {:ok, url_results} ->
+        Logger.info("#{inspect(self())} Inserting #{base_url} to #{@db}:#{@valid_result_table}")
+        insert_results(conn, base_url, url_results)
 
-    # do something
-    crawn(urls)
+      {:error, :bincontent} ->
+        Logger.warning("#{inspect(self())} Binnary content detected (in #{base_url})")
+        Logger.warning("#{inspect(self())} Inserting #{base_url} to #{@db}:#{@binnary_result_table}")
+        Init.insert(conn, @binnary_result_table, base_url)
+
+      {:error, :timeout} ->
+        Logger.error("#{inspect(self())} Timeout error (in #{base_url})")
+        Init.insert(conn, @timeout_error_table, base_url)
+
+      {:error, reason} ->
+        Logger.error("#{inspect(self())} #{reason} (in #{base_url})")
+    end
   end
 
-  def crawn(base_url) do
-    pid = inspect(self())
+  defp insert_results(conn, base_url, url_results) when is_list(url_results) do
+    Logger.info("#{inspect(self())} Found #{length(url_results)} results in #{base_url}")
 
-    if String.valid?(base_url) do
-      Logger.info("#{pid} Working with #{base_url}")
-
-      case ExtractInnerUrls.extract_inner_urls(base_url) do
-        {:ok, urls} ->  #this urls variable IS A LIST of strings
-          Logger.info("#{pid} Found #{length(urls)} results")
-          Logger.info("#{pid} Writting each result to the database")
-          urls
-
-        {:error, reason} ->
-          Logger.error("#{pid} #{base_url} #{reason}")
-          []
-
-        {:warning, reason} ->
-          Logger.warning("#{pid} #{base_url} #{reason}")
-          []
-        end
-
-    else
-      Logger.error("#{pid} Could not handle #{inspect(base_url)} URL string")
-      []
-    end
+    Enum.each(url_results, fn (url) ->
+      case Init.insert(conn, @url_buffer_table, url) do
+        {:ok, _, _, _} ->
+          Logger.info("#{inspect(self())} #{url} inserted on #{@db}:#{@url_buffer_table}")
+        {:error, %Exqlite.Error{message: reason}, _} ->
+          Logger.error("#{inspect(self())} #{reason}")
+      end
+    end)
   end
 end
